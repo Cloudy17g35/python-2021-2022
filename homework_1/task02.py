4e92892373ba045f331ec74be28b00fd101cbf38
Ksawery Lejczak <79964602+Cloudy17g35@users.noreply.github.com>
2021-10-17 22:03:31 +0200

    Update task02.py

--- homework_1/task02.py
+++ homework_1/task02.py
@@ -5,8 +5,21 @@
  Napisz funkcję days_in_year zwracającą liczbę dni w roku (365 albo 366).
 """
 
-def days_in_year(year):
-    pass
+def is_leap(year: int) -> bool:
 
-input = 2019
-output = 365
+    """:argument year: type int type
+        :returns bool True if year is leap False otherwise"""
+
+    if year % 100 == 0 and year % 400:  # years divisible by 100 but not by 400 are not considered as leap
+        return False
+    elif year % 4 == 0:
+        return True
+    return False
+
+
+def days_in_year(year):
+    return 366 if is_leap(year) else 365
+    
+
+inputs = [2019, 2020, 2014, 2100, 2400]
+outputs = [365, 366, 365, 365, 366]
